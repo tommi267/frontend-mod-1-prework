a50cdacd9dbe9b2460ad90099c21df6f96d6b25b
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2021-05-26 17:40:01 -0400

    Add Section 2 work

--- section2/exercises/decision-making.js
+++ section2/exercises/decision-making.js
@@ -38,16 +38,20 @@ if (bearChoice === 1) {
 Questions
 
 1. In English, using technical vocabulary, describe what is happening between lines 14 and 18.
-
+   If doorChoice  is 1 then the bearClothing will be a hat else it'll be a scarf.
 2. What variable has a new value assigned to it after the first if statement executes?
-
+   bearClothing
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
-
+   scarf
 4. In English, using technical vocabulary, describe what is happening between lines lines 27 and 35.
-
+   If bearChoice is 1 the first outcome happens.
+   If bearChoice is 2 the second outcome happens.
+   If bearChoice is 3 the third outcome happens.
+   If bearChoice is anything beside 1,2,or 3 the last outcome happens.
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
-
+   "You run as fast as you can into the next room. It's full of snakes!"
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
-
+   "You tell the bear the hat is too small and it starts to cry!"
 7. What is your favorite ending?
+   The 4th and hidden choice
 */
--- section2/exercises/functions.js
+++ section2/exercises/functions.js
@@ -17,7 +17,7 @@ function printName() {
   console.log("Severus Snape");
 };
 
-printName(); 
+printName();
 printName();
 printName();
 printName();
@@ -27,6 +27,13 @@ printName();
 // YOU DO: Write a function named sayHello that logs to the console "Oh, Hello!"
 // Then, call the function 2 times.
 
+function sayHello() {
+  console.log("Oh, Hello!");
+};
+
+sayHello();
+sayHello();
+
 //-------------------
 // PART 2: Arguments and Parameters
 //-------------------
@@ -35,18 +42,32 @@ printName();
 // The function should print out the value of the String that was passed in.
 // Then, call the function 3 times, each time, passing it a different name.
 
-
+function greetMe(name) {
+  console.log(name);
+};
+greetMe("Kate");
+greetMe("Tom");
+greetMe("Kat");
 
 // YOU DO: Write a function that takes in 2 numbers as arguments, Numbers, and logs their sum
 // Then, call that function 3 times, each time, passing in 2 different Numbers.
 
-
+function sum(num1,num2) {
+  console.log(num1+num2);
+};
+sum(1,5);
+sum(2,6);
+sum(9,4);
 
 // YOU DO🎈: Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
 
+function bestFriends(name1,name2) {
+  console.log(`${name1} and ${name2} are BFFS.`);
+};
 
+bestFriends('Tom','Kate')
 
 //-------------------
 // PART 3: Naming is Hard
@@ -60,6 +81,6 @@ Look at the code you wrote for the previous YOU DO🎈 - what did you name the f
 What did you name each parameter, and why?
 
 EXPLAIN:
-
-
+I name the function bestFriends because the function will take 2 names and log them as bffs.
+For each of the parameter I name them as name1 and name2 because I'm expecting 2 names for this function.
 */
