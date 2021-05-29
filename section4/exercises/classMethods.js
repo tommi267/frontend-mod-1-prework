6af8893f1b10eb5509b349c989a8e7aac46fb326
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2021-05-29 19:10:21 -0400

    Complete classMethods and classProperties

--- section4/exercises/classMethods.js
+++ section4/exercises/classMethods.js
@@ -7,17 +7,17 @@ Complete each task, using the Burrito class below as a starting point.
 Make sure to run the file with node in your command line.
 */
 
-// The burrito should also have a property called `toppings`. It should be assigned to 
+// The burrito should also have a property called `toppings`. It should be assigned to
 // the toppings parameter in the constructor.
 
-// Below/outside of the class declaration, create 3 object instances of a burrito. 
+// Below/outside of the class declaration, create 3 object instances of a burrito.
 // The toppings argument should be an Array of Strings.
 
-// The burrito class should have a method named `changeProtein`. 
+// The burrito class should have a method named `changeProtein`.
 // This method should accept one argument, a String.
 // The method should re-assign this.protein to the value that was passed in.
 
-// Call the `changeProtein` method on a burrito, then log the burrito to verify 
+// Call the `changeProtein` method on a burrito, then log the burrito to verify
 // that the protein has been changed.
 
 // The burrito class should have a method named `addTopping`.
@@ -28,13 +28,34 @@ Make sure to run the file with node in your command line.
 // that the proteins have been changed.
 
 class Burrito {
-  constructor(protein, base) {
+  constructor(protein, base, topping) {
     this.protein = protein;
     this.base = base;
+    this.topping = topping;
   }
-
   // ADD CODE
+  changeProtein(protein){
+    this.protein = protein;
+  }
+  addTopping(toppings){
+    this.topping.push(toppings)
+  }
 };
 
 // ADD CODE
+var burrito1 = new Burrito('chicken','rice',['cheese']);
+console.log(burrito1);
+var burrito2 = new Burrito('beef','beans',[]);
+console.log(burrito2);
+var burrito3 = new Burrito('chicken','beans',['cheese']);
+console.log(burrito3);
 
+burrito1.changeProtein('beef');
+console.log(burrito1);
+burrito2.changeProtein('tofu');
+console.log(burrito2);
+
+burrito2.addTopping('hot sauce');
+console.log(burrito2);
+burrito3.addTopping('hot sauce');
+console.log(burrito3);
--- section4/exercises/classProperties.js
+++ section4/exercises/classProperties.js
@@ -8,7 +8,7 @@ Make sure to run the file with node in your command line.
 //-------------------
 // Part 1: Syntax and Naming Conventions
 //-------------------
-
+/*
 // For each prompt:
   // Write a class skeleton
   // Create 2 object instances and store in variables
@@ -16,11 +16,27 @@ Make sure to run the file with node in your command line.
   // You do not need any proerties at this time!!
 
 // Prompt 1: Dog
-
+class Dog{
+}
+var myDog1 = new Dog();
+var myDog2 = new Dog();
+console.log(myDog1);
+console.log(myDog2);
 // Prompt 2: Snack
-
+class Snack {
+}
+var mySnack1 = new Snack();
+var mySnack2 = new Snack();
+console.log(mySnack1);
+console.log(mySnack2);
 // Prompt 3: Shirt
-
+class Shirt{
+}
+var myShirt1 = new Shirt();
+var myShirt2 = new Shirt();
+console.log(myShirt1);
+console.log(myShirt2);
+*/
 //-------------------
 // Part 2: Properties
 //-------------------
@@ -31,13 +47,38 @@ Make sure to run the file with node in your command line.
   // Assign 3 properties (names and values of your choice) to each class
   // Create an object instance and store in variable
   // Log that variable to the console.
-
+/*
 // Prompt 1: Dog
-
+class Dog {
+  constructor() {
+    this.type = 'Husky';
+    this.age = 2;
+    this.isHungry = true;
+  }
+}
+var dog1 = new Dog();
+console.log(dog1);
 // Prompt 2: Snack
-
+class Snack {
+  constructor() {
+    this.type = 'Potatoe Chips';
+    this.brand = 'Lays';
+    this.flavor = 'Orginal';
+  }
+}
+var snack1 = new Snack();
+console.log(snack1);
 // Prompt 3: Shirt
-
+class Shirt {
+  constructor() {
+    this.size = 'Medium';
+    this.color = 'Black';
+    this.brand = 'Jcrew';
+  }
+}
+var shirt1 = new Shirt();
+console.log(shirt1);
+*/
 //-------------------
 // Part 3: Dynamic Properties
 //-------------------
@@ -51,7 +92,38 @@ Make sure to run the file with node in your command line.
   // Log those variables to the console.
 
 // Prompt 1: Dog
-
+class Dog {
+  constructor(type, age, isHungry) {
+    this.type = type;
+    this.age = age;
+    this.isHungry = isHungry;
+  }
+}
+var dog1 = new Dog('Husky',2,true);
+var dog2 = new Dog('Pitbull',3,false)
+console.log(dog1);
+console.log(dog2);
 // Prompt 2: Snack
-
+class Snack {
+  constructor(type, brand, flavor) {
+    this.type = type;
+    this.brand = brand;
+    this.flavor = flavor;
+  }
+}
+var snack1 = new Snack('Potatoe Chips','Lays','Orginal');
+var snack2 = new Snack('Cookies','Oreo','DoubleStuff');
+console.log(snack1);
+console.log(snack2);
 // Prompt 3: Shirt
+class Shirt {
+  constructor(size, color,brand) {
+    this.size = size;
+    this.color = color;
+    this.brand = brand;
+  }
+}
+var shirt1 = new Shirt('Medium','Black','Jcrew');
+var shirt2 = new Shirt('Large','Pink','Gap');
+console.log(shirt1);
+console.log(shirt2);
